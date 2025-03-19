e62ba76176e7ce583daa38391ee33abc2e85aef8
Kisa234 <110016263+Kisa234@users.noreply.github.com>
2025-03-19 12:14:50 -0500

    adding vite to build

--- src/domain/datasource/reclamacion.datasource.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ReclamacionDatasource = void 0;
-class ReclamacionDatasource {
-}
-exports.ReclamacionDatasource = ReclamacionDatasource;
--- src/domain/repository/reclamacion.repository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ReclamacionRepository = void 0;
-class ReclamacionRepository {
-}
-exports.ReclamacionRepository = ReclamacionRepository;
